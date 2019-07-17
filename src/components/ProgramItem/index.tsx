033a957a3510b6b9d8723bc1d7475ee796c7c193
Burak Kaplan <br.kaplan@hotmail.com>
2019-07-17 21:04:04 +0300

    ProgramItem and ProgramList made responsive

--- src/components/ProgramItem/index.tsx
+++ src/components/ProgramItem/index.tsx
@@ -4,10 +4,8 @@ import { IProgramItemProps } from "../../interfaces";
 
 const ProgramItem: React.FC<IProgramItemProps> = ({ title, url }): JSX.Element => {
   return (
-    <div id="item-wrapper">
-      <div id="poster">
-        <img src={url} alt={title} />
-      </div>
+    <div id="poster">
+      <img src={url} alt={title} />
       <div id="title">
         <span>{title}</span>
       </div>
